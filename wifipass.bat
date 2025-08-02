ec17fe9c0700fad90ff9596566880af34b5157e6
ivosetyadi <ivosetyadi@users.noreply.github.com>
2025-08-02 20:36:29 +0700

    Add files via upload

---
+++ wifipass.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal enabledelayedexpansion
+
+echo ==============================
+echo       WiFi Password List
+echo ==============================
+
+for /f "tokens=2 delims=:" %%i in ('netsh wlan show profiles ^| findstr /C:"All User Profile"') do (
+    set "ssid=%%i"
+    set "ssid=!ssid:~1!"
+    for /f "tokens=2 delims=:" %%j in ('netsh wlan show profile name^="!ssid!" key^=clear ^| findstr /C:"Key Content"') do (
+        set "password=%%j"
+        set "password=!password:~1!"
+        echo !ssid! = !password!
+    )
+)
+
+endlocal
